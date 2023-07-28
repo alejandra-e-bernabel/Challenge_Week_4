b8188c1b5886d96aebc770e26b7c2147fd4e5c5f
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-07-27 23:02:52 -0400

    Added basic button structure and functionality.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-
-
 var timerEl = document.querySelector(".timer");
+//timerEl.style.color = "chartreuse";
+var questionEl = document.getElementById("questionBody");
+var startButton = document.getElementById("startButton");
 
 var secondsLeft = 30;
+var questionNum = 0;
 
+startButton.addEventListener("click",function buttonPress() {
+    questionNum++;
+    printQuestion(questionNum);
+})
 
 //function keeps track of time left in the quiz.
 function setTime() {
@@ -15,7 +21,41 @@ function setTime() {
             clearInterval(timerInterval);
         }
     
-    //the second number here is the delay legnth between calling this code again
+    //the second number here is the delay length between calling this code again
     }, 1000)
 
 }
+
+function printQuestion (i) {
+    questionEl.textContent = "Question 1";
+
+    switch (i) {
+        case 1:
+            questionEl.textContent = "Question 1";
+            startButton.textContent = "Next question";
+            break;
+
+        case 2:
+            questionEl.textContent = "Question 2";
+            break;
+
+        case 3: 
+            questionEl.textContent = "Question 3";
+            break;
+        
+        default:
+            console.log (i);
+            questionEl.textContent = "Something went wrong, please try again."
+            break;
+
+    }
+}
+
+
+// function question1 {
+
+// }
+
+// function question 2 {
+
+// }
